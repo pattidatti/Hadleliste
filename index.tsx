46e614545063f06e3a1f490ca09d9d1d8e30a037
pattidatti <pattidatti@gmail.com>
2026-02-04 11:47:44 +0100

    bugfix

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { ToastProvider } from './components/Toast';
-import './src/index.css';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
